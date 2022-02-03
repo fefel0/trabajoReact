e8579abf3139aa2e5aaad659d550322b165c9af4
Federico Ramallo <federicoramallo96@hotmail.com>
2022-02-03 00:25:25 -0300

    created NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        
+        <nav>
+            <div className='NavLogo'>
+                <img src={'./images/logo.png'} alt='logo' />
+                <ul className='NavItems'>
+                    <li><a href='#'>Productos</a></li>
+                    <li><a href='#'>PC Armadas</a></li>
+                    <li><a href='#'>Notebooks</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+    )
+}
+
+export default NavBar
